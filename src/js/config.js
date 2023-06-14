b5f8ca30ed41b644ec6c5622fc03ef209eac3dd6
Alena Sharai <exultantislupus@gmail.com>
2023-06-14 18:52:27 +0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '457d02b2-7536-45f8-8e5b-7573d3d0b421';
+export const MODAL_CLOSE_SEC = 2.5;
